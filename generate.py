769652cbbcde1d045ad98a91747c9afac9a8d6b3
dzen <dzen@pop-os.localdomain>
2021-10-20 00:57:13 +0100

    Print from pre-determined structure

--- generate.py
+++ generate.py
@@ -12,37 +12,66 @@ x_offset = 0
 y_offset = 0
 z_offset = 5
 
-height = 8
-length = 16
-bredth = 16
+height = 9
+length = 17
+bredth = 17
 
-structure = height * [ length * [ bredth * [False]]]
+structure = height * [ bredth * [ length * [ False ] ] ] 
+#structure = [ [ [ False for _ in range(length) ] for _ in range(bredth) ] for _ in range(height) ]
 
 def main():
-    # initialise the area
+    # Clear the area and place markers
     fill(0,0,0,length,bredth,height,'air')
     plot_point(-1,-1,-1,'red_concrete')
-    plot_point(length+1,bredth+1,height+1,'blue_concrete')
+    plot_point(-1,bredth+1,-1,'red_concrete')
+    plot_point(length+1,-1,-1,'red_concrete')
+    plot_point(length+1,bredth+1,-1,'red_concrete')
+    plot_point(-1,-1,height+1,'blue_concrete')
+    plot_point(-1,bredth+1,height+1,'blue_concrete')
+    plot_point(length+1,-1,height+1,'blue_concrete')
+    plot_point(length+1,-1,height+1,'blue_concrete')
 
 
-    # fill structure with shapes
+    # fill 3D map 'structure' with shapes
     fill_structure()
 
     time.sleep(3)
     for z, z_list in enumerate(structure):
         for y, y_list in enumerate(z_list):
             for x, x_list in enumerate(y_list):
-                print(x,y,z)
                 if(structure[z][y][x]):
                     plot_point(x,y,z)
         time.sleep(1)
 
 
-# This is the function which actually defined the 3d shape
+# This is the function which actually defines the 3D shape
 # Change this according to your needs
+#TODO: Externalisation
 def fill_structure():
-    for z, z_list in enumerate(structure):
-        structure[z] = circle(radius=(int(bredth/2))-z)
+    for z, _ in enumerate(structure):
+        r = radius_at_level(z)
+        print(z,r)
+        structure[z] = moved(circle(radius=r), z)
+
+def radius_at_level(z):
+    return math.floor( math.sqrt ( 8**2 - z**2 ) ) -1 
+
+# ---------------------------------
+
+
+# ---- Utilities --------------
+def moved(matrix, distance):
+    # Moves a given 2D matrix w.r.t origin by a given distance
+    # Returns a new matrix
+    filled = [ [ False for _ in range(length) ] for _ in range(bredth) ]
+    for y,row in enumerate(matrix):
+        for x,_ in enumerate(row):
+            if (x+distance) < length and (y+distance) < bredth:
+                filled[y+distance][x+distance] = matrix[y][x]
+    return filled
+
+# ---------------------------------
+
 
 # ---- Common Shapes --------------
 # Each function returns 2d array
@@ -54,13 +83,10 @@ def circle(radius=1):
     # shifted by r along both axes
     shift = radius
     size = (radius*2) + 1
-    circle = list(map(lambda _:
-            list(map(lambda _: False, range(size))),
-        range(size)))
+    circle = [ [ False for _ in range(size) ] for _ in range(size) ]
     prev_y = radius
     for x in range(radius+1):
         y = math.floor( math.sqrt( (radius**2) - ((x)**2)) )
-        print("Coords y,x:",y,x)
         # Fill main points
         #circle[y][x] = True
         circle[y+shift][x+shift] = True
@@ -94,7 +120,7 @@ def fill(x1, y1, z1, x2, y2, z2, substance=DEFAULT_SUBSTANCE):
     send_cmd("fill {} {} {} {} {} {} {}".format(x1, z1, y1, x2, z2, y2, substance))
 
 def send_cmd(cmd):
-    print(cmd)
+    #print(cmd)
     cmd = cmd + "\\n"
     subprocess.call(["screen", "-S", "minecraft", "-X", "stuff", "{}".format(cmd)])
 
@@ -114,6 +140,5 @@ def print_2d(array_2d):
                 line += "·"
         print(line)
 
-#main()
-print_2d(circle(8))
-
+main()
+#print_2d(moved(circle(4), 0))
