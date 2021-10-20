5dc6b6016648de9c2324c01dc3fd5b807c73da2a
dzen <dzen@pop-os.localdomain>
2021-10-20 02:56:13 +0100

    Removed server.jar from version control

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash 
+set -e
 cd server
+if [[ ! -f server.jar ]]; then
+    curl -vkL 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar' -o server.jar;
+fi
+     
 screen -S minecraft java -Xmx10G -jar server.jar nogui
